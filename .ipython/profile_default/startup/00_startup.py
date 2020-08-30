5169a6fe6fc65e82722fff5edc5d8e072c8b28e4
Charles Fry <52051562+AmishWarlord@users.noreply.github.com>
2020-08-30 19:41:41 -0400

    merged ipython into data analysis

--- .ipython/profile_default/startup/00_startup.py
+++
@@ -1,7 +0,0 @@
-import os
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-from jupyterthemes import jtplot
-jtplot.style(theme='monokai', context='notebook', ticks=True, grid=False)
