cacc04942adc6e4753faef74b5f236a44aa59ffc
Charles Fry <52051562+AmishWarlord@users.noreply.github.com>
2020-09-10 23:15:02 -0400

    Update CumulativeNodeSampling.py

--- modules/CumulativeNodeSampling.py
+++ modules/CumulativeNodeSampling.py
@@ -7,7 +7,7 @@ class CumulativeNodeSampling(BaseEstimator,TransformerMixin) :
     class for Cumulative Node Sampling: a way to drop less-important
     features from a high-dimensional dataset
     """
-    def __init__(self,sample=.9,clf=None):
+    def __init__(self,clf,sample=.9):
         """
 
         :param sample: how much of the explainatory power you want to keep
@@ -22,21 +22,16 @@ class CumulativeNodeSampling(BaseEstimator,TransformerMixin) :
 
     def fit(self, X, y=None):
         sample = self.sample
+        clf = self.clf
 
-        if clf :
-            feats = clf.fit(X,y).feature_importances_
-        else :
-            feats = pd.DataFrame([X,y]).corr(method='spearman').iloc[-1,:-1]
-            feats = np.abs(feats)
+        clf.fit(X,y)
 
+        feats = clf.feature_importances_
+        return feats
         feats = feats / feats.sum()
-        feat_df = pd.DataFrame({
-            'feats':(-feats).argsort(),
-            'values':feats
-        })
-        return feat_df
 
-        feat_df.sort_values(ascending=False,inplace=True)
+
+
 
         num_cols = 1
         score = 0
@@ -54,9 +49,15 @@ class CumulativeNodeSampling(BaseEstimator,TransformerMixin) :
     def transform(self,X,y=None):
         pass
 
-
+from lightgbm import LGBMClassifier
+poop = np.random.randn(5)
+anus = np.array([.9,2.1,2.9,4.4,5.22])
 butt = np.array([1,2,3,4,5])
-print((-butt).argsort())
+X = np.column_stack([poop,anus])
+clf = LGBMClassifier()
+pog = CumulativeNodeSampling(clf)
+poggers = pog.fit(X,butt)
+print(poggers)
 quit()
 
 if __name__ == "__main__":
@@ -68,11 +69,11 @@ if __name__ == "__main__":
 
     X,y = load_iris(return_X_y=True)
 
-    from lightgbm import LGBMClassifier
+
 
     clf = LGBMClassifier()
 
-    sample = CumulativeNodeSampling()
+    sample = CumulativeNodeSampling(clf)
     butt = sample.fit(X,y)
 
     print(butt)
