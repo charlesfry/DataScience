458de8c0ccabc22356ffe824543b4f06d249215a
Charles Fry <AmishWarlord@users.noreply.github.com>
2020-10-06 19:12:09 -0400

    Update deep_tpot.py

--- MechanismsOfAction/deep_tpot.py
+++ MechanismsOfAction/deep_tpot.py
@@ -119,6 +119,7 @@ def build_dicts(tpot_dict, loss_dict, train, targets, reload=False) :
         if col == 'sig_id' : continue
 
         if col in tpot_dict :
+            # pog
             print(f'\tAlready fitted {col} with loss {loss_dict[col]}')
             continue
 
