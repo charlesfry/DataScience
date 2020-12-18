6f560cce495f05693edabbe18f2c11fc79a8d26b
Charles Fry <52051562+AmishWarlord@users.noreply.github.com>
2020-12-18 16:46:44 -0500

    Create bulk_disable.py

---
+++ HelpfulScripts/bulk_disable.py
@@ -0,0 +1,10 @@
+import subprocess
+hand = 'C:\\Temp\disable.csv'
+
+with open(hand, 'r') as file:
+    for line in file:
+        line = line.strip()
+        command = f'Set-User -Identity "{line}" -RemotePowershellEnabled $False'
+
+        p = subprocess.run(["powershell.exe",
+                              command])
