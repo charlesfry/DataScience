d80dc7f4c14d8925ae38b2b285ada9d26aafc201
Charles Fry <52051562+AmishWarlord@users.noreply.github.com>
2021-01-09 16:20:01 -0500

    Update is_prime.py

--- HelpfulScripts/is_prime.py
+++ HelpfulScripts/is_prime.py
@@ -1,20 +1,17 @@
-import math
-
 def is_prime(num:int):
     assert num > 0 and type(num) == int
     if num == 1: return False
     if num == 2: return True
     for i in range(2, int(num / 2) + 1):
         if num % i == 0:
-            print(f'no nerd, it is divisible by {i}')
+            print(f'no, it is divisible by {i}')
             return False
     print(f'{num} is prime')
     return True
 
-def main():
-    print(is_prime(121332951))
+def main(inp):
+    is_prime(inp)
 
 if __name__ == '__main__':
-
-    inp = input('What number should we check?')
-    print(is_prime(int(inp)))
+    inp = int(input('What number should we check?\n'))
+    main(inp)
